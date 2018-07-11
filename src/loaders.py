2179d2795461f7820bdddafef812111a9bc15d1b
Piotr Tarasiewicz <piotr.tarasiewicz@pascal05.intra.codilime.com>
2018-07-11 15:57:38 +0200

    retinanet

--- src/loaders.py
+++ src/loaders.py
@@ -1,5 +1,278 @@
-from .steps.pytorch.loaders import ImageSegmentationBaseDataset
+import os
+from math import sqrt
+import torch
+from attrdict import AttrDict
+from sklearn.externals import joblib
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.sampler import Sampler
+from PIL import Image
+
+from .steps.base import BaseTransformer
+from .utils import meshgrid, box_iou, box_nms, change_box_order
 
 
-class DetectionLoader(ImageSegmentationBaseDataset):
-    pass
+class RandomSubsetSampler(Sampler):
+    def __init__(self, data_size, subset_size):
+        super().__init__()
+        self.data_size = data_size
+        self.subset_size = subset_size
+
+    def __iter__(self):
+        return iter(torch.randperm(self.data_size)[:self.subset_size].long())
+
+    def __len__(self):
+        return self.subset_size
+
+
+class DetectionDataset(Dataset):
+    def __init__(self, X, y, labels, images_dir, encoder, train_mode):
+        super().__init__()
+        self.X = X
+        if y is not None:
+            self.y = y
+            self.labels = labels
+        else:
+            self.y = None
+            self.labels = None
+        self.images_dir = images_dir
+        self.encoder = encoder
+        self.train_mode = train_mode
+
+    def __len__(self):
+        return self.X.shape[0]
+
+    def __getitem__(self, index):
+
+        Xi = self.load_from_disk(index)
+
+        if self.y is not None:
+            Bi, Li = self.load_target(index, Xi.size)
+            return Xi, Bi, Li
+        else:
+            return Xi
+
+    def load_from_disk(self, index):
+        imgId = self.X[index]
+        img_path = os.path.join(self.images_dir, imgId + '.png')
+        return self.load_from_disk(img_path)
+
+    def load_image(self, img_filepath, grayscale=False):
+        image = Image.open(img_filepath, 'r')
+        if not grayscale:
+            image = image.convert('RGB')
+        else:
+            image = image.convert('L')
+        return image
+
+    def load_target(self, index, img_shape):
+        imgId = self.X[index]
+        boxes_rows = self.y[self.y['ImageID'] == imgId]
+        return self.get_boxes_and_labels(boxes_rows, img_shape)
+
+    def get_boxes_and_labels(self, boxes_rows, img_shape):
+        boxes = []
+        labels = []
+        h, w = img_shape
+        for box_row in boxes_rows:
+            x_min = box_row['XMin'].values[0] * h
+            x_max = box_row['XMax'].values[0] * h
+            y_min = box_row['YMin'].values[0] * w
+            y_max = box_row['YMax'].values[0] * w
+            label = box_row['LabelName'].values[0]
+            boxes.append([x_min, y_min, x_max, y_max])
+            labels.append(label)
+        return torch.FloatTensor(boxes), torch.LongTensor(labels)
+
+    def collate_fn(self, batch):
+        '''Encode targets.
+
+        Args:
+          batch: (list) of images, cls_targets, loc_targets.
+
+        Returns:
+          images, stacked cls_targets, stacked loc_targets.
+        '''
+        imgs = [x[0] for x in batch]
+        boxes = [x[1] for x in batch]
+        labels = [x[2] for x in batch]
+
+        inputs = torch.stack(imgs)
+        loc_targets = []
+        cls_targets = []
+        for i in range(len(imgs)):
+            loc_target, cls_target = self.encoder.encode(boxes[i], labels[i], input_size=inputs.size()[2:])
+            loc_targets.append(loc_target)
+            cls_targets.append(cls_target)
+        return inputs, torch.stack(loc_targets), torch.stack(cls_targets)
+
+
+class ImageSegmentationLoaderBasic(BaseTransformer):
+    def __init__(self, labels, loader_params, dataset_params):
+        super().__init__()
+        self.labels = labels
+        self.loader_params = AttrDict(loader_params)
+        self.dataset_params = AttrDict(dataset_params)
+
+        self.encoder = DataEncoder()
+        self.dataset = None
+
+    def transform(self, X, y, X_valid=None, y_valid=None, train_mode=True):
+        if train_mode and y is not None:
+            flow, steps = self.get_datagen(X, y, True, self.loader_params.training)
+        else:
+            flow, steps = self.get_datagen(X, None, False, self.loader_params.inference)
+
+        if X_valid is not None and y_valid is not None:
+            valid_flow, valid_steps = self.get_datagen(X_valid, y_valid, False, self.loader_params.inference)
+        else:
+            valid_flow = None
+            valid_steps = None
+        return {'datagen': (flow, steps),
+                'validation_datagen': (valid_flow, valid_steps)}
+
+    def get_datagen(self, X, y, train_mode, loader_params):
+        if train_mode:
+            dataset = self.dataset(X, y,
+                                   labels=self.labels,
+                                   images_dir=self.dataset_params.images_dir,
+                                   encoder=self.encoder,
+                                   train_mode=True)
+        else:
+            dataset = self.dataset(X, y,
+                                   labels=self.labels,
+                                   images_dir=self.dataset_params.images_dir,
+                                   encoder=self.encoder,
+                                   train_mode=False)
+
+        datagen = DataLoader(dataset, **loader_params, sampler=RandomSubsetSampler(len(dataset), 10))
+        steps = len(datagen)
+        return datagen, steps
+
+    def load(self, filepath):
+        params = joblib.load(filepath)
+        self.loader_params = params['loader_params']
+        return self
+
+    def save(self, filepath):
+        params = {'loader_params': self.loader_params}
+        joblib.dump(params, filepath)
+
+
+class DataEncoder(object):
+    def __init__(self):
+        self.anchor_areas = [32*32., 64*64., 128*128., 256*256., 512*512.]  # p3 -> p7
+        self.aspect_ratios = [1/2., 1/1., 2/1.]
+        self.scale_ratios = [1., pow(2,1/3.), pow(2,2/3.)]
+        self.anchor_wh = self._get_anchor_hw()
+
+    def _get_anchor_hw(self):
+        '''Compute anchor height and width for each feature map.
+
+        Returns:
+          anchor_hw: (tensor) anchor hw, sized [#fm, #anchors_per_cell, 2].
+        '''
+        anchor_hw = []
+        for s in self.anchor_areas:
+            for ar in self.aspect_ratios:  # w/h = ar
+                h = sqrt(s/ar)
+                w = ar * h
+                for sr in self.scale_ratios:  # scale
+                    anchor_h = h*sr
+                    anchor_w = w*sr
+                    anchor_hw.append([anchor_h, anchor_w])
+        num_fms = len(self.anchor_areas)
+        return torch.Tensor(anchor_hw).view(num_fms, -1, 2)
+
+    def _get_anchor_boxes(self, input_size):
+        '''Compute anchor boxes for each feature map.
+
+        Args:
+          input_size: (tensor) model input size of (h, w).
+
+        Returns:
+          boxes: (list) anchor boxes for each feature map. Each of size [#anchors,4],
+                        where #anchors = fmw * fmh * #anchors_per_cell
+        '''
+        num_fms = len(self.anchor_areas)
+        fm_sizes = [(input_size/pow(2.,i+3)).ceil() for i in range(num_fms)]  # p3 -> p7 feature map sizes
+
+        boxes = []
+        for i in range(num_fms):
+            fm_size = fm_sizes[i]
+            grid_size = input_size / fm_size
+            fm_h, fm_w = int(fm_size[0]), int(fm_size[1])
+            xy = meshgrid(fm_h,fm_w) + 0.5  # [fm_h*fm_w, 2]
+            xy = (xy*grid_size).view(fm_w,fm_h,1,2).expand(fm_w,fm_h,9,2)
+            hw = self.anchor_wh[i].view(1,1,9,2).expand(fm_w,fm_h,9,2)
+            box = torch.cat([xy,hw], 3)  # [x,y,w,h]
+            boxes.append(box.view(-1,4))
+        return torch.cat(boxes, 0)
+
+    def encode(self, boxes, labels, input_size):
+        '''Encode target bounding boxes and class labels.
+
+        We obey the Faster RCNN box coder:
+          tx = (x - anchor_x) / anchor_w
+          ty = (y - anchor_y) / anchor_h
+          tw = log(w / anchor_w)
+          th = log(h / anchor_h)
+
+        Args:
+          boxes: (tensor) bounding boxes of (xmin,ymin,xmax,ymax), sized [#obj, 4].
+          labels: (tensor) object class labels, sized [#obj,].
+          input_size: (int/tuple) model input size of (w,h).
+
+        Returns:
+          loc_targets: (tensor) encoded bounding boxes, sized [#anchors,4].
+          cls_targets: (tensor) encoded class labels, sized [#anchors,].
+        '''
+        input_size = torch.Tensor([input_size,input_size]) if isinstance(input_size, int) \
+                     else torch.Tensor(input_size)
+        anchor_boxes = self._get_anchor_boxes(input_size)
+        boxes = change_box_order(boxes, 'xyxy2xywh')
+
+        ious = box_iou(anchor_boxes, boxes, order='xywh')
+        max_ious, max_ids = ious.max(1)
+        boxes = boxes[max_ids]
+
+        loc_xy = (boxes[:,:2]-anchor_boxes[:,:2]) / anchor_boxes[:,2:]
+        loc_wh = torch.log(boxes[:,2:]/anchor_boxes[:,2:])
+        loc_targets = torch.cat([loc_xy,loc_wh], 1)
+        cls_targets = 1 + labels[max_ids]
+
+        cls_targets[max_ious<0.5] = 0
+        ignore = (max_ious>0.4) & (max_ious<0.5)  # ignore ious between [0.4,0.5]
+        cls_targets[ignore] = -1  # for now just mark ignored to -1
+        return loc_targets, cls_targets
+
+    def decode(self, loc_preds, cls_preds, input_size):
+        '''Decode outputs back to bouding box locations and class labels.
+
+        Args:
+          loc_preds: (tensor) predicted locations, sized [#anchors, 4].
+          cls_preds: (tensor) predicted class labels, sized [#anchors, #classes].
+          input_size: (int/tuple) model input size of (w,h).
+
+        Returns:
+          boxes: (tensor) decode box locations, sized [#obj,4].
+          labels: (tensor) class labels for each box, sized [#obj,].
+        '''
+        CLS_THRESH = 0.5
+        NMS_THRESH = 0.5
+
+        input_size = torch.Tensor([input_size,input_size]) if isinstance(input_size, int) \
+                     else torch.Tensor(input_size)
+        anchor_boxes = self._get_anchor_boxes(input_size)
+
+        loc_xy = loc_preds[:,:2]
+        loc_wh = loc_preds[:,2:]
+
+        xy = loc_xy * anchor_boxes[:,2:] + anchor_boxes[:,:2]
+        wh = loc_wh.exp() * anchor_boxes[:,2:]
+        boxes = torch.cat([xy-wh/2, xy+wh/2], 1)  # [#anchors,4]
+
+        score, labels = cls_preds.sigmoid().max(1)          # [#anchors,]
+        ids = score > CLS_THRESH
+        ids = ids.nonzero().squeeze()             # [#obj,]
+        keep = box_nms(boxes[ids], score[ids], threshold=NMS_THRESH)
+        return boxes[ids][keep], labels[ids][keep]
---
+++ src/retinanet.py
@@ -0,0 +1,251 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torchvision import models
+
+from .utils import one_hot_embedding
+
+
+class FPN(nn.Module):
+    def __init__(self, encoder_depth, pretrained=True):
+        super().__init__()
+
+        if encoder_depth == 34:
+            self.encoder = models.resnet34(pretrained=pretrained)
+            bottom_channel_nr = 512
+        elif encoder_depth == 50:
+            self.encoder = models.resnet50(pretrained=pretrained)
+            bottom_channel_nr = 2048
+        elif encoder_depth == 101:
+            self.encoder = models.resnet101(pretrained=pretrained)
+            bottom_channel_nr = 2048
+        elif encoder_depth == 152:
+            self.encoder = models.resnet152(pretrained=pretrained)
+            bottom_channel_nr = 2048
+        else:
+            raise Exception('ResNet depth must be in {34, 50, 101, 152}')
+
+        self.pool = nn.MaxPool2d(2, 2)
+
+        self.relu = nn.ReLU(inplace=True)
+
+        self.conv1 = nn.Sequential(self.encoder.conv1,
+                                   self.encoder.bn1,
+                                   self.encoder.relu,
+                                   self.pool)
+        self.conv2 = self.encoder.layer1
+        self.conv3 = self.encoder.layer2
+        self.conv4 = self.encoder.layer3
+        self.conv5 = self.encoder.layer4
+
+        self.conv6 = nn.Conv2d(bottom_channel_nr, 256, kernel_size=3, stride=2, padding=1)
+        self.conv7 = nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1)
+
+        # Lateral layers
+        self.latlayer1 = nn.Conv2d(bottom_channel_nr, 256, kernel_size=1, stride=1, padding=0)
+        self.latlayer2 = nn.Conv2d(bottom_channel_nr // 2, 256, kernel_size=1, stride=1, padding=0)
+        self.latlayer3 = nn.Conv2d(bottom_channel_nr // 4, 256, kernel_size=1, stride=1, padding=0)
+
+        # Top-down layers
+        self.toplayer1 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.toplayer2 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.toplayer3 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+
+    def _upsample_add(self, x, y):
+        '''Upsample and add two feature maps.
+
+        Args:
+          x: (Variable) top feature map to be upsampled.
+          y: (Variable) lateral feature map.
+
+        Returns:
+          (Variable) added feature map.
+
+        Note in PyTorch, when input size is odd, the upsampled feature map
+        with `F.upsample(..., scale_factor=2, mode='nearest')`
+        maybe not equal to the lateral feature map size.
+
+        e.g.
+        original input size: [N,_,15,15] ->
+        conv2d feature map size: [N,_,8,8] ->
+        upsampled feature map size: [N,_,16,16]
+
+        So we choose bilinear upsample which supports arbitrary output sizes.
+        '''
+        _, _, H, W = y.size()
+        return F.upsample(x, size=(H, W), mode='bilinear') + y
+
+    def forward(self, x):
+        # Bottom-up
+        c1 = self.conv1(x)
+        c2 = self.conv2(c1)
+        c3 = self.conv3(c2)
+        c4 = self.conv4(c3)
+        c5 = self.conv5(c4)
+
+        p6 = self.conv6(c5)
+        p7 = self.conv7(F.relu(p6))
+
+        # Top-down
+        p5 = self.latlayer1(c5)
+        p4 = self._upsample_add(p5, self.latlayer2(c4))
+        p3 = self._upsample_add(p4, self.latlayer3(c3))
+
+        p5 = self.toplayer1(p5)
+        p4 = self.toplayer2(p4)
+        p3 = self.toplayer3(p3)
+
+        return p3, p4, p5, p6, p7
+
+
+class RetinaNet(nn.Module):
+    """PyTorch RetinaNet model using ResNet(34, 50, 101 or 152 backbone).
+
+    Paper: https://arxiv.org/pdf/1708.02002.pdf
+    Original PyTorch implementation: https://github.com/kuangliu/pytorch-retinanet
+
+    Args:
+        encoder_depth (int): Depth of ResNet backbone, must be in {34, 50, 101, 152}.
+        pretrained_encoder (bool): If True weights pretrained on ImageNet will be used for ResNet. Defaults to True.
+        num_classes (int): Number of classes for detection. Defaults to 20.
+    """
+    num_anchors = 9
+
+    def __init__(self, encoder_depth, pretrained_encoder=True, num_classes=20):
+        super(RetinaNet, self).__init__()
+        self.fpn = FPN(encoder_depth, pretrained_encoder)
+        self.num_classes = num_classes
+        self.loc_head = self._make_head(self.num_anchors * 4)
+        self.cls_head = self._make_head(self.num_anchors * self.num_classes)
+
+    def forward(self, x):
+        fms = self.fpn(x)
+        loc_preds = []
+        cls_preds = []
+        for fm in fms:
+            loc_pred = self.loc_head(fm)
+            cls_pred = self.cls_head(fm)
+            loc_pred = loc_pred.permute(0, 2, 3, 1).contiguous().view(x.size(0), -1,
+                                                                      4)  # [N, 9*4,H,W] -> [N,H,W, 9*4] -> [N,H*W*9, 4]
+            cls_pred = cls_pred.permute(0, 2, 3, 1).contiguous().view(x.size(0), -1,
+                                                                      self.num_classes)  # [N,9*20,H,W] -> [N,H,W,9*20] -> [N,H*W*9,20]
+            loc_preds.append(loc_pred)
+            cls_preds.append(cls_pred)
+        return torch.cat(loc_preds, 1), torch.cat(cls_preds, 1)
+
+    def _make_head(self, out_planes):
+        layers = []
+        for _ in range(4):
+            layers.append(nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1))
+            layers.append(nn.ReLU(True))
+        layers.append(nn.Conv2d(256, out_planes, kernel_size=3, stride=1, padding=1))
+        return nn.Sequential(*layers)
+
+    def freeze_bn(self):
+        '''Freeze BatchNorm layers.'''
+        for layer in self.modules():
+            if isinstance(layer, nn.BatchNorm2d):
+                layer.eval()
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, num_classes=20):
+        super(FocalLoss, self).__init__()
+        self.num_classes = num_classes
+
+    def focal_loss(self, x, y):
+        '''Focal loss.
+
+        Args:
+          x: (tensor) sized [N,D].
+          y: (tensor) sized [N,].
+
+        Return:
+          (tensor) focal loss.
+        '''
+        alpha = 0.25
+        gamma = 2
+
+        t = one_hot_embedding(y.data.cpu(), 1+self.num_classes)  # [N,21]
+        t = t[:,1:]  # exclude background
+        t = Variable(t).cuda()  # [N,20]
+
+        p = x.sigmoid()
+        pt = p*t + (1-p)*(1-t)         # pt = p if t > 0 else 1-p
+        w = alpha*t + (1-alpha)*(1-t)  # w = alpha if t > 0 else 1-alpha
+        w = w * (1-pt).pow(gamma)
+        return F.binary_cross_entropy_with_logits(x, t, w, size_average=False)
+
+    def focal_loss_alt(self, x, y):
+        '''Focal loss alternative.
+
+        Args:
+          x: (tensor) sized [N,D].
+          y: (tensor) sized [N,].
+
+        Return:
+          (tensor) focal loss.
+        '''
+        alpha = 0.25
+
+        t = one_hot_embedding(y.data.cpu(), 1+self.num_classes)
+        t = t[:,1:]
+        t = Variable(t).cuda()
+
+        xt = x*(2*t-1)  # xt = x if t > 0 else -x
+        pt = (2*xt+1).sigmoid()
+
+        w = alpha*t + (1-alpha)*(1-t)
+        loss = -w*pt.log() / 2
+        return loss.sum()
+
+    def forward(self, loc_preds, loc_targets, cls_preds, cls_targets):
+        '''Compute loss between (loc_preds, loc_targets) and (cls_preds, cls_targets).
+
+        Args:
+          loc_preds: (tensor) predicted locations, sized [batch_size, #anchors, 4].
+          loc_targets: (tensor) encoded target locations, sized [batch_size, #anchors, 4].
+          cls_preds: (tensor) predicted class confidences, sized [batch_size, #anchors, #classes].
+          cls_targets: (tensor) encoded target labels, sized [batch_size, #anchors].
+
+        loss:
+          (tensor) loss = SmoothL1Loss(loc_preds, loc_targets) + FocalLoss(cls_preds, cls_targets).
+        '''
+        batch_size, num_boxes = cls_targets.size()
+        pos = cls_targets > 0  # [N,#anchors]
+        num_pos = pos.data.long().sum()
+
+        ################################################################
+        # loc_loss = SmoothL1Loss(pos_loc_preds, pos_loc_targets)
+        ################################################################
+        mask = pos.unsqueeze(2).expand_as(loc_preds)       # [N,#anchors,4]
+        masked_loc_preds = loc_preds[mask].view(-1,4)      # [#pos,4]
+        masked_loc_targets = loc_targets[mask].view(-1,4)  # [#pos,4]
+        loc_loss = F.smooth_l1_loss(masked_loc_preds, masked_loc_targets, size_average=False)
+
+        ################################################################
+        # cls_loss = FocalLoss(loc_preds, loc_targets)
+        ################################################################
+        pos_neg = cls_targets > -1  # exclude ignored anchors
+        mask = pos_neg.unsqueeze(2).expand_as(cls_preds)
+        masked_cls_preds = cls_preds[mask].view(-1,self.num_classes)
+        cls_loss = self.focal_loss_alt(masked_cls_preds, cls_targets[pos_neg])
+
+        print('loc_loss: %.3f | cls_loss: %.3f' % (loc_loss.data[0]/num_pos, cls_loss.data[0]/num_pos), end=' | ')
+        loss = (loc_loss+cls_loss)/num_pos
+        return loss
+
+
+if __name__ == "__main__":
+    num_classes = 500
+    net = RetinaNet(encoder_depth=50,
+                    pretrained_encoder=True,
+                    num_classes=num_classes)
+    loc_preds, cls_preds = net(Variable(torch.randn(2, 3, 256, 256)))
+    print(loc_preds.size())
+    print(cls_preds.size())
+    loc_grads = Variable(torch.randn(loc_preds.size()))
+    cls_grads = Variable(torch.randn(cls_preds.size()))
+    loc_preds.backward(loc_grads)
+    cls_preds.backward(cls_grads)
